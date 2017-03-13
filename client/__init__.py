17407b40dc2b283ed9b4b4c74eb34d1f6f5c1f74
Rudy <rudyroggio97@gmail.com>
2017-03-13 13:29:52 -0400

    Update __init__.py
    Didn't change much. Relatively nothing. Removed some stuff, fewer lines. Runs and works the same.

--- client/__init__.py
+++ client/__init__.py
@@ -13,14 +13,10 @@
 # limitations under the License.
 
 import sys
-x=sys.modules['containerregistry.client']
-  
-
-from containerregistry.client import docker_name_
-setattr(x, 'docker_name', docker_name_)
-
-
 from containerregistry.client import docker_creds_
+from containerregistry.client import docker_name_
+x=sys.modules['containerregistry.client']
+setattr(x, 'docker_name', docker_name_)
 setattr(x, 'docker_creds', docker_creds_)
 
 
